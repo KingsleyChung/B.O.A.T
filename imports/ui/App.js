4c66c50e1f1235dad49cd122f3ba0644943ca20e
KingsleyChung <kingsleychung@outlook.com>
2017-12-25 10:40:38 +0800

    sync

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -4,16 +4,18 @@ import { Apps } from '../api/collection.js'
 
 class App extends Component {
     render() {
-        if (this.props.app[0])
         return ( 
             <div className="app-container">
-                <div className="wechat-cover">
-                    <h1 className="title-text">{this.props.app[0].name}</h1>
+                <div className="app-cover">
+                    <img className="iPhone-frame" src="image/iPhoneFrame.png"/>
+                    <img className="app-capture" src="image/QinGongShanXueCapture.jpg"/>
+                    <img className="app-code" src={this.props.app[0] && this.props.app[0].code}/>
+                    <h1 className="app-title-text">{this.props.app[0] && this.props.app[0].name}</h1>
                 </div>
                 <div className="app-info">
-                    <img className="app-icon" src={this.props.app[0].icon}/>
-                    <img className="app-code" src={this.props.app[0].code}/>
-                    <span className="app-description">{this.props.app[0].description}</span>
+                    <button className="go-back">GO BACK</button>
+                    <img className="app-icon" src={this.props.app[0] && this.props.app[0].icon}/>
+                    <span className="app-description">{this.props.app[0] && this.props.app[0].description}</span>
                 </div>
             </div>
             
@@ -24,6 +26,6 @@ class App extends Component {
 export default withTracker(() => {
     Meteor.subscribe('apps');
     return {
-        app: Apps.find({name: "WeChat"}).fetch(),
+        app: Apps.find({name: "勤工善学"}).fetch(),
     };
 })(App);
